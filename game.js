a032772f653b137a66ac32d5e5b75af50c4ddcc2
norest <orestisef@gmail.com>
2019-01-22 21:49:07 +0200

    Simon Game Done!

--- game.js
+++ game.js
@@ -1,6 +1,17 @@
 var userClickedPattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var started = false;
+var level = 0;
+
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
 
 
 $(".btn").click(function() {
@@ -11,11 +22,40 @@ $(".btn").click(function() {
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
 });
+
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    var audio = new Audio("sounds/wrong.mp3");
+    audio.play();
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver() ;
+  }
+
+}
 
 
 
 function nextSequence() {
+
+  userClickedPattern = [];
+  $("#level-title").text("Level " + level);
+  level++;
 
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
@@ -29,6 +69,8 @@ function nextSequence() {
 }
 
 
+
+
 function playSound(name) {
 
   var audio = new Audio("sounds/" + name + ".mp3");
@@ -38,7 +80,13 @@ function playSound(name) {
 function animatePress(currentColour) {
   $("#" + currentColour).addClass("pressed");
 
-  setTimeout(function(){
-            $("#" + currentColour).removeClass("pressed");
-    }, 100);
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function startOver() {
+  level =0;
+  gamePattern =[];
+  started = false;
 }
