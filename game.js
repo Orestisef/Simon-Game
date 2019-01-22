ecb2aa0df0ed846cf223b2e2ebff356d7760ecce
norest <orestisef@gmail.com>
2019-01-22 20:35:03 +0200

    event hundler

--- game.js
+++ game.js
@@ -1,6 +1,17 @@
+var userClickedPattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
-
 var gamePattern = [];
+
+
+$(".btn").click(function(){
+
+  var userChosenColour = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColour);
+
+});
+
+
 
 function nextSequence() {
 
@@ -8,8 +19,9 @@ function nextSequence() {
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
+
 }
